46d62bf1096570c8ea97c5ef46af0d0a32f443f2
Mohammed Alshanqiti <lsm4682a@etu.iut-tlse3.fr>
2024-12-03 15:59:15 +0100

    commit finale

--- app/src/main/java/com/example/kotlin_apk/Main.kt
+++ app/src/main/java/com/example/kotlin_apk/Main.kt
@@ -7,7 +7,6 @@ fun main() {
     }
     println(ystudent)
     println(morestudent)
-    println(allstudent)
 }
 
 abstract class Piece() {
@@ -50,4 +49,4 @@ val etudiants = listOf(
 
 val ystudent = etudiants.find { etudiant -> etudiant.promo == "2024" }?.name
 val morestudent = etudiants.filter { it.matieres.size > 2 }.map { it.name }
-val allstudent = etudiants.filter { it.matieres.isNotEmpty() }.map { it.matieres.size }.map {  }
+val allstudent = etudiants.filter { it.matieres.isNotEmpty() }.map { it.matieres.size }
